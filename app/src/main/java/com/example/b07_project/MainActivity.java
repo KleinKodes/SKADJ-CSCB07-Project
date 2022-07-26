136a206e931381bdf0e8fa8a8f16ab1c8ae3f407
Seven <seven_jang@yahoo.ca>
2022-07-26 19:21:48 -0400

    Created Login and Registration Activity
    1.    I created the Login and Registration Activity
    2.   I made it so that every time you open the app, the Login and Registration Activity Appears

--- app/src/main/java/com/example/b07_project/MainActivity.java
+++ app/src/main/java/com/example/b07_project/MainActivity.java
@@ -6,12 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.b07_project.ui.login.LoginActivity;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent1 = new Intent(this, LoginActivity.class);
+        startActivity(intent1);
     }
 
     public void transitionToAddEvent(View view)
