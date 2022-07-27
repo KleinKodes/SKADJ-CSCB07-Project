74e6efe85c4d29d59e6f2ffe00c8c0a3dddfec6e
KleinKodes <kleinharrigan@gmail.com>
2022-07-26 21:38:26 -0400

    Revert "Figured out how to properly set event id's, edited class data a little"
    This reverts commit bc58451cb20769c08b00f98b1f6e03dbc8e9703c.

--- app/src/main/java/com/example/b07_project/Venue.java
+++ app/src/main/java/com/example/b07_project/Venue.java
@@ -2,7 +2,6 @@ package com.example.b07_project;
 
 import android.location.Address;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Venue {
@@ -10,39 +9,8 @@ public class Venue {
     String name;
     String description;
     Address address;
-    ArrayList<String> sports;
+    List<String> sports;
     int capacity;
-    ArrayList<Integer> scheduledEvents;
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public Address getAddress() {
-        return address;
-    }
-
-    public ArrayList<String> getSports() {
-        return sports;
-    }
-
-    public int getCapacity() {
-        return capacity;
-    }
-
-    public ArrayList<Integer> getScheduledEvents() {
-        return scheduledEvents;
-    }
-
-
 
     public Venue(){
 
