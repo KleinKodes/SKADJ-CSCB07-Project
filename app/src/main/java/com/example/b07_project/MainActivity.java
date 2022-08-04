4ef7d7692ea662f4d596c1d9aaa272ae9fe392e5
KleinKodes <kleinharrigan@gmail.com>
2022-08-04 14:31:58 -0400

    Fixed bug in navigation class

--- app/src/main/java/com/example/b07_project/MainActivity.java
+++ app/src/main/java/com/example/b07_project/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity { //
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.loading_activity);
 
 
        Intent intent = new Intent(this, WrapperActivity.class);
